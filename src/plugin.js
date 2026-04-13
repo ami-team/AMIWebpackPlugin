3c3aa109bcb5c2755244cba736bb2151225918b7
hmangione <hugo.mangione@lpsc.in2p3.fr>
2026-04-13 11:36:27 +0200

    add rules

--- src/plugin.js
+++ src/plugin.js
@@ -1,5 +1,16 @@
 /*--------------------------------------------------------------------------------------------------------------------*/
 
+const BROWSER_LIST = [
+	'defaults',
+	'not ie 11',
+	'not ie_mob 11'
+];
+/*--------------------------------------------------------------------------------------------------------------------*/
+
+console.log(`Building for: ${BROWSER_LIST.join(', ')}`);
+
+/*--------------------------------------------------------------------------------------------------------------------*/
+
 // noinspection JSUnusedGlobalSymbols
 export default class AMIWebpackPlugin
 {
@@ -8,15 +19,94 @@ export default class AMIWebpackPlugin
 	// noinspection JSUnusedGlobalSymbols
 	apply(compiler)
 	{
-		/*------------------------------------------------------------------------------------------------------------*/
-		/* EXTERNALS                                                                                                  */
-		/*------------------------------------------------------------------------------------------------------------*/
+		if(typeof compiler.options === 'undefined') {
+			compiler.options = {};
+		}
+
+		if(typeof compiler.options.module === 'undefined') {
+			compiler.options.module = {};
+		}
+
+		if(typeof compiler.options.module.rules === 'undefined') {
+			compiler.options.module.rules = [];
+		}
 
 		if(typeof compiler.options.externals === 'undefined') {
 			compiler.options.externals = {};
 		}
 
+		if(typeof compiler.options.resolve.alias === 'undefined') {
+			compiler.options.resolve.alias = {};
+		}
+
+		/*------------------------------------------------------------------------------------------------------------*/
+		/* RULES                                                                                                      */
+		/*------------------------------------------------------------------------------------------------------------*/
+
+		compiler.options.module.rules.push({
+			test: /\\.vue$/,
+			loader: 'vue-loader'
+		});
+
+		compiler.options.module.rules.push({
+			'test': /\\.js$/,
+			'use': {
+				'loader': 'babel-loader',
+				'options': {
+					'shouldPrintComment': () => false,
+					'plugins': [
+						['@babel/plugin-transform-for-of', {
+							'loose': true
+						}]
+					],
+					'presets': [
+						['@babel/preset-env', {
+							'loose': true,
+							'targets': BROWSER_LIST
+						}]
+					]
+				}
+			}
+		});
+
+		compiler.options.module.rules.push({
+			'type': 'asset/source',
+			'test': /\\.(twig|json|yml|xml)$/,
+			'exclude': /node_modules/
+		});
+
+		compiler.options.module.rules.push({
+			'type': 'asset/resource',
+			'test': /\\.(gif|png|jpg|jpeg|svg|wasm)$/,
+			'exclude': /node_modules/
+		});
+
+		compiler.options.module.rules.push({
+			test: /\\.css$/,
+			use: [
+				'style-loader',
+				'css-loader',
+				{
+					'loader': 'postcss-loader',
+					'options': {
+						'postcssOptions': {
+							'plugins': [
+								['autoprefixer', {}]
+							]
+						}
+					}
+				}
+			]
+		});
+
+		/*------------------------------------------------------------------------------------------------------------*/
+		/* EXTERNALS                                                                                                  */
+		/*------------------------------------------------------------------------------------------------------------*/
+
 		compiler.options.externals['ami'] = 'ami';
+		compiler.options.externals['jquery'] = 'jQuery';
+		compiler.options.externals['moment'] = 'moment';
+		compiler.options.externals['select2'] = 'select2';
 
 		compiler.options.externals['vue'] = ['ami', 'vue', 'Vue'];
 		compiler.options.externals['vue/dist/vue.esm-bundler'] = ['ami', 'vue', 'Vue'];
@@ -27,10 +117,6 @@ export default class AMIWebpackPlugin
 		/*------------------------------------------------------------------------------------------------------------*/
 		/* ALIASES                                                                                                    */
 		/*------------------------------------------------------------------------------------------------------------*/
-
-		if(typeof compiler.options.resolve.alias === 'undefined') {
-			compiler.options.resolve.alias = {};
-		}
 
 		compiler.options.resolve.alias['ami'] = false;
 
